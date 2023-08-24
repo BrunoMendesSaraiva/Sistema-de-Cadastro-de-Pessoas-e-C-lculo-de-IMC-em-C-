c899feeb32d1c3f33b230b8de0e712efd73d9a00
bruno mendes <brunombdsaraiva@gmail.com>
2023-08-23 22:56:04 -0300

    Atualização

--- Trabalho.cpp
+++ Trabalho.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <conio.h>
 #include <locale.h>
+#include <stdexcept>
 
 // Definição da estrutura Pessoa
 struct Pessoa {
@@ -17,25 +18,78 @@ float calcularIMC(float peso, float altura) {
     return peso / (altura * altura);
 }
 
+// Função para verificar se a string contém apenas letras
+bool contemApenasLetras(const std::string& str) {
+    for (char c : str) {
+        if (!std::isalpha(c) && !std::isspace(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 // Função para cadastrar uma pessoa e armazená-la internamente
 void cadastrarPessoa(std::vector<Pessoa>& pessoas) {
     Pessoa pessoa;
 
-    std::cout << "Digite o nome: ";
-    std::cin.ignore(); // Limpar o buffer do teclado
-    std::getline(std::cin, pessoa.nome);
+    try{
+        std::cout << "Digite o nome: ";
+        std::cin.ignore();
+        std::getline(std::cin, pessoa.nome);
 
-    std::cout << "Digite a idade: ";
-    std::cin >> pessoa.idade;
+        if (!contemApenasLetras(pessoa.nome)) {
+            throw std::invalid_argument("Nome inválido. O nome deve conter apenas letras.");
+        }
 
-    std::cout << "Digite o peso (em kg): ";
-    std::cin >> pessoa.peso;
+        if (pessoa.nome.length() < 3 || pessoa.nome.length() > 50) {
+            throw std::invalid_argument("Nome inválido. O nome deve ter entre 3 e 50 letras.");
+        }
 
-    std::cout << "Digite a altura (em metros): ";
-    std::cin >> pessoa.altura;
+        if (!std::isupper(pessoa.nome[0])) {
+            throw std::invalid_argument("Nome inválido. A primeira letra deve ser maiúscula.");
+        }
 
-    pessoas.push_back(pessoa);
-    std::cout << "Pessoa cadastrada com sucesso!\n";
+        std::cout << "Digite a idade: ";
+        std::cin >> pessoa.idade;
+        if (pessoa.idade <= 0 || pessoa.idade > 150) {
+            throw std::invalid_argument("Idade inválida. A idade deve ser positiva e razoável.");
+        }
+
+        std::cout << "Digite o peso (em kg): ";
+        std::string pesoStr;
+        std::cin >> pesoStr;
+        try {
+            pessoa.peso = std::stof(pesoStr);
+            if (pessoa.peso <= 0 || pessoa.peso > 500) {
+                throw std::invalid_argument("Peso inválido. O peso deve ser positivo e razoável.");
+            }
+            if (pessoa.peso <= 0) {
+                throw std::invalid_argument("Peso inválido. O peso deve ser positivo.");
+            }
+        }
+        catch (const std::exception& e) {
+            throw std::invalid_argument("Peso inválido. Use '.' como separador decimal.");
+        }
+
+        std::cout << "Digite a altura (em metros): ";
+        std::string alturaStr;
+        std::cin >> alturaStr;
+        try {
+            pessoa.altura = std::stof(alturaStr);
+            if (pessoa.altura <= 0 || pessoa.altura > 3) {
+                throw std::invalid_argument("Altura inválida. A altura deve ser positiva e razoável.");
+            }
+        } catch (const std::exception& e) {
+            throw std::invalid_argument("Altura inválida. Use '.' como separador decimal.");
+        }
+
+        pessoas.push_back(pessoa);
+        std::cout << "Pessoa cadastrada com sucesso!\n";
+    }
+    catch (const std::exception& e) {
+        std::cout << "Erro ao cadastrar pessoa: " << e.what() << std::endl;
+    }
 }
 
 // Função para listar pessoas cadastradas com nomes, idades e IMCs
@@ -64,11 +118,28 @@ void pesquisarPorNome(const std::vector<Pessoa>& pessoas, const std::string& nom
         std::cout << "\nPessoa não encontrada.\n";  
     }
 }
+// Função para limpar o console
 int sair() {
     std::cout << "\n \n \n";
     system("PAUSE");
     system("cls");
     return 0;
+}
+
+// Função para imprimir observação baseada no IMC
+void imprimirObservacaoIMC(float imc) {
+    if (imc < 18.5) {
+        std::cout << "Obs: Você está abaixo do peso\n";
+    }
+    else if (imc < 24) {
+        std::cout << "Obs: Você está com peso normal\n";
+    }
+    else if (imc < 29) {
+        std::cout << "Obs: Você está acima do peso\n";
+    }
+    else {
+        std::cout << "Obs: Infelizmente, você está obeso\n";
+    }
 }
 
 int main() {
